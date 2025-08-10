3f9b902778e6b746ab9f1e7ed94ca5498bc65116
rohanmathur91 <mathurrohan04@gmail.com>
2025-08-10 16:43:16 +0530

    remove dist

--- packages/core/dist/esm/core.d.ts
+++
@@ -1,6 +0,0 @@
-export declare function createStore<State, Action>(initialState: State, reducers: (state: State, action: Action) => State): {
-    getState: () => State;
-    subscribe: (cb: (state: State) => void) => () => boolean;
-    dispatch: (action: Action) => void;
-};
-//# sourceMappingURL=core.d.ts.map
