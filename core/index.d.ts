39032064a142f1682e9d1b0f83272259a59af881
rohanmathur91 <mathurrohan04@gmail.com>
2025-08-10 16:46:31 +0530

    cleanup

--- core/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare const createStore: <State, Action>(initialState: State, reducers: (state: State, action: Action) => State) => {
-    getState: () => State;
-    subscribe: (cb: (state: State) => void) => () => boolean;
-    dispatch: (action: Action) => void;
-};
-//# sourceMappingURL=index.d.ts.map
